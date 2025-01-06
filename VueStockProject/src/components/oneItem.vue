72c674ac034d1bdd10914f79f4cf798184f8af33
osundstrom <oskar.sundstrom123@gmail.com>
2025-01-06 21:55:33 +0100

    lagt till stockitem, visar all items. lagt till card. laggt till bilder för exemepl.

---
+++ VueStockProject/src/components/oneItem.vue
@@ -0,0 +1,63 @@
+<script>
+    export default {
+        name: "oneItem",
+        props:  {
+            oneItem: Object
+        },
+        mounted() {
+    console.log(this.oneItem); 
+  }
+    }
+    
+
+</script>
+
+<template>
+<div id ="cardDiv" class="col-lg-3 col-md-4 col-sm-6 col-12 mb-4 d-flex justify-content-center flex-wrap ">
+<div class="card " style="width: 18rem;">
+    <div id="imageDiv">
+  <img v-bind:src="oneItem.itemImage" class="card-img-top" id="squareImage" alt="picture of product">
+</div>
+  <div class="card-body">
+    <h5 class="card-title">{{oneItem.itemCode}} </h5>
+    <p class="card-text"> Märke: {{oneItem.itemBrand}} </p>
+    <p class="card-text"> Saldo:{{oneItem.itemStock}} </p>
+    <p class="card-text"> Pris: {{oneItem.itemPrice}} </p>
+    <button type="button" class="btn btn-danger"><i class="fa-regular fa-trash-can fa-sm"></i></button>
+    <button type="button" class="btn btn-warning"><i class="fa-regular fa-pen-to-square fa-sm"></i></button>
+  </div>
+</div>
+</div>
+
+ 
+
+</template>
+
+<style scoped>
+
+
+#squareImage {
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
+object-fit: cover;
+}
+
+#imageDiv{
+position: relative;
+width: 100%;
+padding-top: 100%;    
+overflow: hidden;
+}
+
+.card-body{
+    text-align: center;
+}
+
+#cardDiv {
+    padding: 1vw; 
+}
+
+</style>
--- VueStockProject/src/views/StockView.vue
+++ VueStockProject/src/views/StockView.vue
@@ -1,21 +1,70 @@
 <script>
 import navigation from '../components/navigation.vue'
+import oneItem from '../components/oneItem.vue'
 
+let url = "http://localhost:3000/item";
 
 export default {
-    components: {navigation,}
+    data(){
+    return {
+      allItems:[]
+      
+    }
+  },
+    components: {navigation,oneItem},
+    
+    methods: {
+        async getItems() {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                console.log(data);
+                this.allItems = data;
+                
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        },
+        mounted() {
+            this.getItems();
+    }
+    
   }
 
 </script>
 
 <template>
 
-<navigation/>
-<h1>Lagersaldo</h1>
-  
+<main>
+    <navigation/>
+<h1 class="d-flex justify-content-center">Lagersaldo</h1>
+    <div id="divItems" class="container " >   
+        <div class="row d-flex justify-content-start">
+            
+        <oneItem v-for="oneItem in allItems" :key="oneItem._id" :oneItem="oneItem" />
+    
+    </div>
+    </div>
+    
+</main>
 </template>
 
 <style scoped>
+main{
+    background-image: url("/src/assets/backMain.jpg");
+    background-size: cover; 
+    background-position: center; 
+    min-height: 100vh;
+    color: white;
+    
+}
+
+
+#divitems{
+    margin-top: 2rem;
+}
+
 
 </style>
 
